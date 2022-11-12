406bda7b73742ae1f0d82d654953c58a2ce8353c
Seechange <danh.vo.2110@gmail.com>
2022-11-12 16:08:10 +0700

    check input js

--- js/signup.js
+++ js/signup.js
@@ -41,3 +41,60 @@ function login(){
         }
     }  
 }
+const email = document.querySelector("#email");
+const phoneNumber = document.querySelector("#phonenumber")
+const iconError = document.querySelector(".iconError");
+const iconSuccess = document.querySelector(".iconSuccess");
+const error = document.querySelector(".error-text");
+let regExp = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
+let vnf_regex = /((09|03|07|08|05)+([0-9]{8})\b)/g;
+function check(key,fun){
+  if(key.value.match(fun)){
+    key.style.borderColor = "#27ae60";
+    key.style.background = "#eafaf1";
+    iconError.style.display = "none";
+    iconSuccess.style.display = "block";
+    error.style.display = "none";
+  }else{
+    key.style.borderColor = "#e74c3c";
+    key.style.background = "#fceae9";
+    iconError.style.display = "block";
+    iconSuccess.style.display = "none";
+    error.style.display = "block";
+  }
+  if(key.value == ""){
+    key.style.borderColor = "lightgrey";
+    key.style.background = "#fff";
+    iconError.style.display = "none";
+    iconSuccess.style.display = "none";
+    error.style.display = "none";
+  }
+}
+    // var mobile = $('#mobile').val();
+    function checkPhone(key){
+        if(key !==''){
+            if (vnf_regex.test(key.value)  == false) 
+            {
+                alert("Số điện thoại của bạn không đúng định dạng")
+                // key.style.borderColor = "#e74c3c";
+                // key.style.background = "#fceae9";
+                // iconError.style.display = "block";
+                // iconSuccess.style.display = "none";
+                // error.style.display = "block";
+            }else{
+                alert("Số điện thoại của bạn hợp lệ")
+                // key.style.borderColor = "#27ae60";
+                // key.style.background = "#eafaf1";
+                // iconError.style.display = "none";
+                // iconSuccess.style.display = "block";
+                // error.style.display = "none";
+            }
+        }else{
+            alert("Bạn chưa điền số điện thoại")
+            // key.style.borderColor = "lightgrey";
+            // key.style.background = "#fff";
+            // iconError.style.display = "none";
+            // iconSuccess.style.display = "none";
+            // error.style.display = "none";
+        }
+    }
